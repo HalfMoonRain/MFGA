0592e1ae285a86bdf25e5601e5714e68db547dd3
JaePar <hawni77@gmail.com>
2024-10-15 15:56:04 +0900

    Replace "payment_frequency" with "m"

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ class Item(BaseModel):
     n: float # 만기(년)
     R: float # 액면이자율(1년)
     YTM: float # 만기수익률(1년)
-    payment_frequency: int # 이자지급횟수(회1년 , ex  2 = 6개월마다 지급, 4 = 3개월마다 지급)
+    m: int # 이자지급횟수(회1년 , ex  2 = 6개월마다 지급, 4 = 3개월마다 지급)
 
 @app.get("/")
 def root():
@@ -16,13 +16,13 @@ def root():
 
 
 @app.get("/calculate", summary="현재가치 계산")
-def calculate(FV: float, n: int, R: float, YTM: float, payment_frequency: int):
+def calculate(FV: float, n: int, R: float, YTM: float, m: int):
     """
     # 이자의 연금현재가치계산과 원금의 현재가치계산을 합계하여 채권의 현재가치 출력
     """
-    adjusted_R = R / payment_frequency  # 주기별 이자율
-    adjusted_YTM = YTM / payment_frequency  # 주기별 수익률
-    total_periods = n * payment_frequency  # 총 이자 지급 횟수
+    adjusted_R = R / m  # 주기별 이자율
+    adjusted_YTM = YTM / m  # 주기별 수익률
+    total_periods = n * m  # 총 이자 지급 횟수
     
     interest = calculate_sum_fv_r(FV, adjusted_R, adjusted_YTM, total_periods)
     principal = calculate_fv_single(FV, adjusted_YTM, total_periods)
